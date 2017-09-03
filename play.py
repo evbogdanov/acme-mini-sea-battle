5454c8e7ce7c39134fde72f47274a6f54e64c266
Ev Bogdanov <evbogdanov.com@gmail.com>
2017-09-04 01:02:46 +0300

    Event handling

--- play.py
+++ play.py
@@ -1,7 +1,56 @@
 #!/usr/bin/env python3
 
 import subprocess
+import re
+
+# TODO: don't use `os`
 import os
+
+## ACME EVENT
+## -----------------------------------------------------------------------------
+
+class Event:
+	def __init__(self, event_bytes):
+		valid, orig, etype, beg, end, text = self.parse_event(event_bytes)
+		self.valid = valid
+		self.orig = orig
+		self.etype = etype
+		self.beg = beg
+		self.end = end
+		self.text = text
+
+	@classmethod
+	def parse_event(cls, event_bytes):
+		"""
+		Parse window event.
+		
+		Valid event looks like:
+		"event M L 1 1 0 3 2 4 Text '' ''"
+		
+		See `acmeevent(1)` for details
+		"""
+
+		valid = False
+		orig = ''
+		etype = ''
+		beg = ''
+		end = ''
+		text = ''
+
+		event_str = event_bytes.decode().rstrip()		
+		match = re.match(
+			r"^event (\w+) (\w+) (\d+) (\d+) \d+ \d+ \d+ \d+ (.+) '' ''$",
+			event_str)
+
+		if match:
+			valid = True
+			orig = match.group(1)
+			etype = match.group(2)
+			beg = match.group(3)
+			end = match.group(4)
+			text = match.group(5) 
+
+		return valid, orig, etype, beg, end, text
 
 ## ACME WINDOW
 ## -----------------------------------------------------------------------------
@@ -41,7 +90,9 @@ class Window:
 		"""
 		message = ' '.join(messages)
 		proc1 = subprocess.Popen(['echo', message], stdout=subprocess.PIPE)
-		proc2 = subprocess.Popen(['9p', 'write', f'acme/{self.id}/ctl'], stdin=proc1.stdout, stdout=subprocess.PIPE)
+		proc2 = subprocess.Popen(
+			['9p', 'write', f'acme/{self.id}/ctl'],
+			stdin=proc1.stdout, stdout=subprocess.PIPE)
 		proc2.communicate()
 
 	def clean(self):
@@ -55,7 +106,9 @@ class Window:
 		Append text to the window's body
 		"""
 		proc1 = subprocess.Popen(['echo', text], stdout=subprocess.PIPE)
-		proc2 = subprocess.Popen(['9p', 'write', f'acme/{self.id}/body'], stdin=proc1.stdout, stdout=subprocess.PIPE)
+		proc2 = subprocess.Popen(
+			['9p', 'write', f'acme/{self.id}/body'],
+			stdin=proc1.stdout, stdout=subprocess.PIPE)
 		proc2.communicate()
 
 	def clear(self):
@@ -65,15 +118,32 @@ class Window:
 		os.system(f"echo '' | tr '\n' ',' | 9p write acme/{self.id}/addr")
 		os.system(f"9p write acme/{self.id}/data </dev/null")
 
-## PLAY
+	def listen(self, handler):
+		"""
+		Start listening for events
+		"""
+		pipe = subprocess.Popen(
+			f'9p read acme/{self.id}/event 2>/dev/null | acmeevent',
+			shell=True, stdout=subprocess.PIPE).stdout
+		for event_bytes in pipe:
+			event = Event(event_bytes)
+			handler(event)
+
+## GAMEPLAY
 ## -----------------------------------------------------------------------------
 
 def play():
+	def event_handler(event):
+		if event.valid:
+			print(f"Valid event: {event.orig} {event.etype} '{event.text}'")
+		else:
+			print('Invalid event')
+
 	w = Window()
 	print(w.id)
 	w.append('One\nTwo')
-#	w.clear()
 	w.clean()
+	w.listen(event_handler)
 
 if __name__ == '__main__':
 	play()
