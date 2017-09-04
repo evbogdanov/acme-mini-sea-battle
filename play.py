21dd411f5577d4ac827765ac5ab3c13cd99ede9c
Ev Bogdanov <evbogdanov.com@gmail.com>
2017-09-04 18:37:20 +0300

    Grid class

--- play.py
+++ play.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import re
+import random
 
 # TODO: get rid of all `os.system`, use `subprocess` instead 
 import os
@@ -157,12 +158,47 @@ class Window:
 			event = Event(event_bytes)
 			handler(event)
 
+## GRID
+## -----------------------------------------------------------------------------
+
+class Grid:
+	LETTERS = 'ABCD'
+	NUMBERS = '1234'
+
+	SQUARE_EMPTY = ' '
+	SQUARE_SHIP = 'x'
+	SQUARE_MISS = '-'
+	SQUARE_HIT = '+'
+
+	NUMBER_OF_SHIPS = 4
+
+	def __init__(self):
+		squares = {}
+		for coord in self.coordinates():
+			squares[coord] = self.SQUARE_EMPTY
+		self.squares = squares
+
+	@classmethod
+	def coordinates(cls):
+		return [f'{let}{num}' for let in cls.LETTERS for num in cls.NUMBERS]
+
+	def squares_at_line(self, num):
+		"""
+		Make printing line by line easy peasy
+		"""
+		squares_list = []
+		for let in self.LETTERS:
+			squares_list.append(self.squares[f'{let}{num}'])
+		return squares_list
+
 ## GAME
 ## -----------------------------------------------------------------------------
 
 class Game:
 	def __init__(self):
 		self.window = Window()
+		self.grid_player = Grid()
+		self.grid_bot = Grid()
 
 	def event_handler(self, event):
 		"""
@@ -184,9 +220,47 @@ class Game:
 
 	def play(self):
 		"""
-		Let's play
+		Start playing
 		"""
+		self.put_bot_ships()
+		self.print()
 		self.window.listen(self.event_handler)
+
+	def print(self):
+		"""
+		Print the state of the game to the window
+		"""
+		self.window.clear()
+		squares = (
+			self.grid_bot.squares_at_line(1) +
+			self.grid_player.squares_at_line(1) +
+			self.grid_bot.squares_at_line(2) +
+			self.grid_player.squares_at_line(2) +
+			self.grid_bot.squares_at_line(3) +
+			self.grid_player.squares_at_line(3) +
+			self.grid_bot.squares_at_line(4) +
+			self.grid_player.squares_at_line(4))
+		self.window.append("""
+    A   B   C   D            A   B   C   D
+  +---+---+---+---+        +---+---+---+---+
+1 | {} | {} | {} | {} |      1 | {} | {} | {} | {} |
+  +---+---+---+---+        +---+---+---+---+
+2 | {} | {} | {} | {} |      2 | {} | {} | {} | {} |
+  +---+---+---+---+        +---+---+---+---+
+3 | {} | {} | {} | {} |      3 | {} | {} | {} | {} |
+  +---+---+---+---+        +---+---+---+---+
+4 | {} | {} | {} | {} |      4 | {} | {} | {} | {} |
+  +---+---+---+---+        +---+---+---+---+""".format(*squares))
+		self.window.clean()
+
+	def put_bot_ships(self):
+		"""
+		Randomly put bot ships on its grid
+		"""
+		coords = self.grid_bot.coordinates()
+		random.shuffle(coords)
+		for coord in coords[:Grid.NUMBER_OF_SHIPS]:
+			self.grid_bot.squares[coord] = Grid.SQUARE_SHIP
 
 ## MAIN
 ## -----------------------------------------------------------------------------
